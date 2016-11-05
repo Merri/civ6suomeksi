3a2db772d48b32d7731717d24ac9b3c8f9f0128f
Vesa Piittinen <vesa.piittinen@gmail.com>
2016-11-05 21:47:56 +0200

    Add localization database SQL

---
+++ LocalizationDatabase_Data.sql
@@ -0,0 +1,5 @@
+INSERT INTO "Languages" VALUES('fi_FI','Suomi',null,2);
+INSERT INTO "DefaultAudioLanguages" VALUES('fi_FI','English(US)');
+INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
+INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
+INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
