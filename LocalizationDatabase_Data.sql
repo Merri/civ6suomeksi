d2e7d5818a56521c9891254a55414997e7519cd2
Vesa Piittinen <vesa.piittinen@gmail.com>
2016-11-06 16:14:45 +0200

    Get rid of German

--- LocalizationDatabase_Data.sql
+++ LocalizationDatabase_Data.sql
@@ -3,3 +3,8 @@ INSERT INTO "DefaultAudioLanguages" VALUES('fi_FI','English(US)');
 INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
 INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
 INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
+
+DELETE FROM "Languages" WHERE Locale = 'de_DE';
+DELETE FROM "SteamLanguages" WHERE Locale = 'de_DE';
+DELETE FROM "LanguagePriorities" WHERE PrimaryLanguage = 'de_DE';
+DELETE FROM "FontStyleSheets" WHERE Language = 'de_DE';
