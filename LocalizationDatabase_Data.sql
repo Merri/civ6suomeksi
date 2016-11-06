c5147bd874bce4d0ca2813778334fecd6ecb6de1
Vesa Piittinen <vesa.piittinen@gmail.com>
2016-11-06 21:31:51 +0200

    Resolve the language fallback issue

--- LocalizationDatabase_Data.sql
+++ LocalizationDatabase_Data.sql
@@ -1,14 +1,14 @@
+-- Other languages become the default fallback so remove (what is the point of priorities if they have no effect?) --
+DELETE FROM "Languages" WHERE Locale != 'en_US';
+DELETE FROM "AudioLanguages" WHERE Locale != 'en_US';
+DELETE FROM "DefaultAudioLanguages" WHERE Locale != 'en_US';
+DELETE FROM "SteamLanguages" WHERE Locale != 'en_US';
+DELETE FROM "LanguagePriorities" WHERE PrimaryLanguage != 'en_US';
+DELETE FROM "FontStyleSheets" WHERE Language != 'en_US';
+DELETE FROM "LocalizedText" WHERE Language != 'en_US';
+
 INSERT INTO "Languages" VALUES('fi_FI','Suomi',null,2);
 INSERT INTO "DefaultAudioLanguages" VALUES('fi_FI','English(US)');
 INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
 INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
 INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
-
-# German becomes the default fallback so remove it
-DELETE FROM "Languages" WHERE Locale = 'de_DE';
-DELETE FROM "AudioLanguages" WHERE Locale = 'de_DE';
-DELETE FROM "DefaultAudioLanguages" WHERE Locale = 'de_DE';
-DELETE FROM "SteamLanguages" WHERE Locale = 'de_DE';
-DELETE FROM "LanguagePriorities" WHERE PrimaryLanguage = 'de_DE';
-DELETE FROM "FontStyleSheets" WHERE Language = 'de_DE';
-DELETE FROM "LocalizedText" WHERE Language = 'de_DE';
