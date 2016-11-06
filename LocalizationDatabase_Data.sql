945ad978d37218c49fe754f367de27792c102251
Vesa Piittinen <vesa.piittinen@gmail.com>
2016-11-07 00:58:58 +0200

    Fix language priorities, use a trick to make Finnish to top choice in the game's language menu

--- LocalizationDatabase_Data.sql
+++ LocalizationDatabase_Data.sql
@@ -1,14 +1,9 @@
--- Other languages become the default fallback so remove (what is the point of priorities if they have no effect?) --
-DELETE FROM "Languages" WHERE Locale != 'en_US';
-DELETE FROM "AudioLanguages" WHERE Locale != 'en_US';
-DELETE FROM "DefaultAudioLanguages" WHERE Locale != 'en_US';
-DELETE FROM "SteamLanguages" WHERE Locale != 'en_US';
-DELETE FROM "LanguagePriorities" WHERE PrimaryLanguage != 'en_US';
-DELETE FROM "FontStyleSheets" WHERE Language != 'en_US';
-DELETE FROM "LocalizedText" WHERE Language != 'en_US';
+-- Add so that language is at the top of the list --
+UPDATE "Languages" SET Locale = 'fi_FI', Name = 'Suomi' WHERE Locale = 'en_US';
+INSERT INTO "Languages" VALUES('en_US','English',null,2);
 
-INSERT INTO "Languages" VALUES('fi_FI','Suomi',null,2);
 INSERT INTO "DefaultAudioLanguages" VALUES('fi_FI','English(US)');
 INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
 INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
+INSERT INTO "LanguagePriorities" VALUES('fi_FI','en_US',90);
 INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
