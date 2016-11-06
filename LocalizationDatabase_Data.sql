09ca8bb112360529a8cddae2bd95a928ade8dc96
Vesa Piittinen <vesa.piittinen@gmail.com>
2016-11-06 17:11:36 +0200

    Further German removal

--- LocalizationDatabase_Data.sql
+++ LocalizationDatabase_Data.sql
@@ -4,7 +4,11 @@ INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
 INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
 INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
 
+# German becomes the default fallback so remove it
 DELETE FROM "Languages" WHERE Locale = 'de_DE';
+DELETE FROM "AudioLanguages" WHERE Locale = 'de_DE';
+DELETE FROM "DefaultAudioLanguages" WHERE Locale = 'de_DE';
 DELETE FROM "SteamLanguages" WHERE Locale = 'de_DE';
 DELETE FROM "LanguagePriorities" WHERE PrimaryLanguage = 'de_DE';
 DELETE FROM "FontStyleSheets" WHERE Language = 'de_DE';
+DELETE FROM "LocalizedText" WHERE Language = 'de_DE';
