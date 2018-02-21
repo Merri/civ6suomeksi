5f1977dc00027b063f425988b2037ee75679414f
Vesa Piittinen <vesa.piittinen@gmail.com>
2018-02-21 17:02:40 +0200

    Major refactor: the project should now work both as a Mod and via user Text folder

--- LocalizationDatabase_Data.sql
+++
@@ -1,9 +0,0 @@
--- Add so that language is at the top of the list --
-UPDATE "Languages" SET Locale = 'fi_FI', Name = 'Suomi' WHERE Locale = 'en_US';
-INSERT INTO "Languages" VALUES('en_US','English',null,2,1);
-
-INSERT INTO "DefaultAudioLanguages" VALUES('fi_FI','English(US)');
-INSERT INTO "SteamLanguages" VALUES('finnish','fi_FI');
-INSERT INTO "LanguagePriorities" VALUES('fi_FI','fi_FI',100);
-INSERT INTO "LanguagePriorities" VALUES('fi_FI','en_US',90);
-INSERT INTO "FontStyleSheets" VALUES('fi_FI','Civ6_FontStyles_EFIGS.xml', 'MinionPro-Medium.otf');
